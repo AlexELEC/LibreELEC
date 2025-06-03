2560b9feb3f25a4523abc0d223fba639becb806f
alexelec <alexelecv5@gmail.com>
2025-06-03 06:56:06 +0000

    pvr.tvlink: init pkg

---
+++ packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2011-present AlexELEC (https://alexelec.tv)
+
+PKG_NAME="pvr.tvlink"
+PKG_VERSION="22.0.0"
+PKG_SHA256="a94da030b973c67c4c6265c795a2a211a5588ff88bd52ff3879b0497dfdacca1"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/AlexELEC/pvr.tvlink"
+PKG_URL="${PKG_SITE}/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform pugixml zlib xz"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.tvlink"
+PKG_LONGDESC="pvr.tvlink"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
