dd0be8c74f55284060e0350fb4227353eb887e73
alexelec <alexelecv5@gmail.com>
2025-06-05 09:06:23 +0000

    pvr.tvlink: update

--- packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2011-present AlexELEC (https://alexelec.tv)
+# Copyright (C) 2011-present AlexELEC (https://alexelec.github.io)
 
 PKG_NAME="pvr.tvlink"
 PKG_VERSION="22.0.1"
-PKG_SHA256="16da247b94f3aeb09cb900aceb2027ca94c9da4809cd138befdb70cadd908dc5"
-PKG_REV="0"
+PKG_SHA256="52cce544a3cd66072acb0c127f2fc697d838acfc847d262c9bf839ac09bc188a"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AlexELEC/pvr.tvlink"
