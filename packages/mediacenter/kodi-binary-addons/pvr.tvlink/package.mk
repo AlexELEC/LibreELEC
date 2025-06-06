6a5022a2567dca90893ccc240931b2270b9f7174
alexelec <alexelecv5@gmail.com>
2025-06-06 13:14:28 +0000

    pvr.tvlink: use non sysroot xz

--- packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
@@ -4,8 +4,8 @@
 
 PKG_NAME="pvr.tvlink"
 PKG_VERSION="22.0.1"
-PKG_SHA256="d472cd77ef0d06bb9e096e0f11b541093566ef138d3bef7bf438f104d4ab1a3a"
-PKG_REV="0"
+PKG_SHA256="352bab93adec890cf6c8132900e8813077b7dffbb3f6633ff0b3f5cdd56038ff"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AlexELEC/pvr.tvlink"
@@ -17,3 +17,9 @@ PKG_LONGDESC="pvr.tvlink"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.pvrclient"
+
+pre_configure_target() {
+  ls -la  ../Findlzma.cmake
+  sed -i -e "s#^find_path(LZMA_INCLUDE_DIRS lzma.h#find_path(LZMA_INCLUDE_DIRS lzma.h PATHS $(get_install_dir xz)/usr/include NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH#" \
+         -e "s#^find_library(LZMA_LIBRARIES NAMES lzma liblzma#find_library(LZMA_LIBRARIES NAMES lzma liblzma PATHS $(get_install_dir xz)/usr/lib NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH#" ../Findlzma.cmake
+}
